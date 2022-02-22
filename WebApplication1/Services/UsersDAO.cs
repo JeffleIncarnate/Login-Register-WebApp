24009c070854eb8f701324e327d747f72a698cbd
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-02-23 10:38:09 +1300

    Commented the USERSDAO.cs

--- WebApplication1/Services/UsersDAO.cs
+++ WebApplication1/Services/UsersDAO.cs
@@ -18,29 +18,38 @@ namespace WebApplication1.Services
            // This is the SQL statement in a varaible so we dont need to type it out over and over again
             string sqlStatement = "SELECT * FROM dbo.Users WHERE username = @username AND password = @password";
 
+            // Using the connection string we can connect to the server 
             using (SqlConnection connection = new SqlConnection(connectionString)) 
             {
+                // This is a Constructor that instantiates the new Object
                 SqlCommand command = new SqlCommand(sqlStatement, connection);
 
+                // This is the command that GETS the datat from the SQL serevr
                 command.Parameters.Add("@username", System.Data.SqlDbType.VarChar, 40).Value = user.UserName;
                 command.Parameters.Add("@password", System.Data.SqlDbType.VarChar, 40).Value = user.Password;
 
+                // This is a try catch block that Catches the error in case the server messed up
                 try
                 {
+                    // This opens the server
                     connection.Open();
                     SqlDataReader reader = command.ExecuteReader();
 
+                    // This checks if the server
                     if (reader.HasRows)
                     {
                         sucess = true;
                     }
                 } 
+                // This is a catch block that catches the errors from the server
                 catch (Exception e)
                 {
+                    // This catches the server error and writes it to the console
                     Console.WriteLine(e.Message);
                 }
             }
 
+            // This returns the varaible back to the function
             return sucess;
         }
     }
